9e20aff9e0aaa1d7dfc846ec05b5f27558efb9d3
Tomas Acuña Ruz <Tartomas@users.noreply.github.com>
2016-11-14 00:38:00 -0300

    Update global.r

--- global.r
+++ global.r
@@ -1,8 +1,8 @@
 #library(data.table)
 library(shiny)
-library(shinyBS)
-library(shinyjs)
-library(shinydashboard)
+#library(shinyBS)
+#library(shinyjs)
+#library(shinydashboard)
 
 datos<-read.csv("elements.csv")
 
